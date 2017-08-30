3e574b0638d4f76148f58278379e4c640d606a61
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-08-30 15:11:37 +0300

    fix: add context argument to registry client

--- client/consul/index.js
+++ client/consul/index.js
@@ -1,12 +1,18 @@
 const consul = require('consul');
 const Client = require('../index');
 class ConsulClient extends Client {
-    constructor(config) {
-        super(config);
-        this.consul = consul(Object.assign({}, config, {promisify: true}));
+    init() {
+        this.consul = consul({
+            host: this.config.host || '127.0.0.1',
+            port: this.config.port || '8500',
+            promisify: true
+        });
+        return this.consul.agent.self();
     }
 
     serviceAdd(config) {
+        var context = Object.assign({}, this.context, config.context);
+        config.tags = Object.keys(context).map(key => `${key}=${context[key]}`);
         return this.consul.agent.service.register(config);
     }
 
@@ -15,6 +21,21 @@ class ConsulClient extends Client {
             service: criteria.name
         })
         .then((services) => {
+            delete criteria.name;
+            let criteriaKeys = Object.keys(criteria);
+            if (criteriaKeys.length) {
+                var result = services.reduce((all, service) => {
+                    if (criteriaKeys.every(key => {
+                        return service.ServiceTags.find(tag => {
+                            return tag === `${key}=${criteria[key]}`;
+                        })
+                    })) {
+                        all.push(service);
+                    }
+                    return all;
+                }, []);
+                return result;
+            }
             return services.map((service) => {
                 return {
                     host: service.ServiceAddress,
