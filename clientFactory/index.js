d1345f4d55f12619b504353ff5a9933912d1d0d5
Krasimir Kostadinov <krasimir.kostadinov@Softwaregroup-bg.com>
2017-10-26 17:01:14 +0300

    fix: typo
    Signed-off-by: Krasimir Kostadinov <krasimir.kostadinov@Softwaregroup-bg.com>

--- clientFactory/index.js
+++ clientFactory/index.js
@@ -17,7 +17,7 @@ module.exports = (clientConfig) => {
     config.type = config.options.watch ? `${config.type}Watch` : config.type;
     let Constructor = clients[config.type];
     if (!Constructor) {
-        throw new Error(`Uknown registry type: ${config.type}! Available types: consul, ut}`);
+        throw new Error(`Unknown registry type: ${config.type}! Available types: consul, ut}`);
     }
     return new Constructor(config.config, config.context, config.options);
 };
